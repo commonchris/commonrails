3580cf9f9aac7909e79bec4519b92f90aa4b6e0c
Chris Hill <chris.hill@commonconcept.net>
2012-01-05 19:56:00 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commonconcept::Application.config.secret_token = 'd358f78408539f0759cea663502d01c991d507a74bbebe9d882d131f95f7bfaa2013f79ca52bd010d60de99370b0a7f3d9477a7ff1e5d0b0f383b4e395f38e5f'
